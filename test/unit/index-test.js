302be933e9ae9f0ab925af116426b74c609cac43
kanekotic <alvarojosepl@gmail.com>
2018-08-27 08:53:57 +0200

    add context

--- test/unit/index-test.js
+++ test/unit/index-test.js
@@ -1,11 +1,23 @@
 import {worker} from '../../lib'
 
 describe('index should', () => {
-    test('construct using library', async () => {
+    test('call simple stateless function', async () => {
         global.postMessage = jest.fn()
         let params ={ data : [{
             functionString: 'function (a){ return a }',
             args: 2
+        }]} 
+        worker(params)
+        expect(global.postMessage).toBeCalledWith([2])
+    })
+
+    test('call simple function with context', async () => {
+        var myDemo = new Demo()
+        global.postMessage = jest.fn()
+        let params ={ data : [{
+            functionString: 'function (a){ return a }',
+            args: 2,
+            context: undefined
         }]} 
         worker(params)
         expect(global.postMessage).toBeCalledWith([2])
