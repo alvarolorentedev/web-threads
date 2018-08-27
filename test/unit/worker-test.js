0cc78054639670050e4b311e1a46fff641337bc2
kanekotic <alvarojosepl@gmail.com>
2018-08-27 21:04:50 +0200

    changed to apply to be able to pass multiple parameters

---
+++ test/unit/worker-test.js
@@ -0,0 +1,75 @@
+import {worker} from '../../lib/worker'
+
+function Func(value){
+    this.value = value
+}
+
+describe('index should', () => {
+    beforeAll(() => {
+        global.postMessage = jest.fn()
+    })
+    beforeEach(() => {
+        global.postMessage.mockReset()
+    })
+    test('call simple stateless function', async () => {
+        let func = (value) => {
+            return value * value
+        }
+        let params ={ data : [{
+            functionString: func.toString(),
+            args: [2]
+        }]} 
+        worker(params)
+        expect(global.postMessage).toBeCalledWith([4])
+    })
+
+    test('call function with context', async () => {
+        Func.prototype.foo = function(){
+            return this.value * this.value
+        };
+        
+        var instance = new Func(2)
+        let params ={ data : [{
+            functionString: instance.foo.toString(),
+            context: instance
+        }]} 
+        worker(params)
+        expect(global.postMessage).toBeCalledWith([4])
+    })
+
+    test('call function with context and param', async () => {
+        
+        Func.prototype.foo2 = function(otherValue){
+            return this.value * otherValue
+        };
+        
+        var instance = new Func(2)
+        let params ={ data : [{
+            functionString: instance.foo2.toString(),
+            context: instance,
+            args: [4]
+        }]} 
+        worker(params)
+        expect(global.postMessage).toBeCalledWith([8])
+    })
+
+    test('call class with context and param', async () => {
+        class someClass {
+            constructor(val){
+                this.val = val
+            }
+            foo(some){
+                return this.val * some
+            }
+        }
+        
+        var instance = new someClass(2)
+        let params ={ data : [{
+            functionString: instance.foo.toString(),
+            context: instance,
+            args: [2]
+        }]} 
+        worker(params)
+        expect(global.postMessage).toBeCalledWith([4])
+    })
+})
