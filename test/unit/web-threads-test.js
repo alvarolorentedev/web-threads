d467da1fa05b97dea42fff17ff3cfdd63c84d61e
kanekotic <alvarojosepl@gmail.com>
2018-08-29 09:15:42 +0200

    added test for call without context resolution

--- test/unit/web-threads-test.js
+++ test/unit/web-threads-test.js
@@ -7,7 +7,7 @@ import faker from 'faker'
 
 describe('web threads should', () => {
 
-    test('call returns result', async () => {
+    test('call with context returns result', async () => {
         let fakeResult = faker.random.uuid()
         let fakeContext = {some: faker.random.uuid()}
         const webWorkerMock = {
@@ -24,6 +24,24 @@ describe('web threads should', () => {
         result = await result
         expect(result).toEqual(fakeResult)
         expect(params.context).toEqual(fakeContext)
+    })
+
+    test('call without context returns result', async () => {
+        let fakeResult = faker.random.uuid()
+        let fakeContext = {some: faker.random.uuid()}
+        const webWorkerMock = {
+            postMessage: () => {webWorkerMock.onmessage({data: [fakeResult,fakeContext]})},
+        }
+        global.Worker = jest.fn().mockImplementation(() => webWorkerMock)
+        global.URL = {createObjectURL: jest.fn()}
+        let params = {
+            fn: () => {return 2},
+            args: [2]
+        }
+        let result = execute(params)
+        result = await result
+        expect(result).toEqual(fakeResult)
+        expect(params.context).toBeUndefined()
     })
 
     test('call fails', async () => {
