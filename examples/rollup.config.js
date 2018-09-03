dd44ef7ef2b2353639bedc853441298d77dae78d
kanekotic <alvarojosepl@gmail.com>
2018-09-03 08:06:10 +0200

    improve scripts to remove manual need of rollup cli for integration test

--- examples/rollup.config.js
+++ examples/rollup.config.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel'
 
 export default {
-    input: "./examples/index.js",
+    input: "examples/index.js",
     plugins: [
         babel({
             "presets": [
