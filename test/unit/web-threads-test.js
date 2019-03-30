80a81b0a5d10948660a46b461c1a31e5a531d8f3
kanekotic <alvarojosepl@gmail.com>
2019-03-30 10:29:09 +0100

    ongoing changes

--- test/unit/web-threads-test.js
+++ test/unit/web-threads-test.js
@@ -1,60 +1,60 @@
-// jest.mock('../../lib/worker', () => () => {`onmessage = function worker(params){if(params.error){throw "error"} postMessage([params.data[0].args, params.data[0].context])} return`})
-// import worker from '../../lib/worker'
-// import { execute } from '../../lib/web-threads'
-// import faker from 'faker'
+jest.mock('../../lib/worker', () => () => {`onmessage = function worker(params){if(params.error){throw "error"} postMessage([params.data[0].args, params.data[0].context])} return`})
+import worker from '../../lib/worker'
+import { execute } from '../../lib/web-threads'
+import faker from 'faker'
 
 
 
 describe('web threads should', () => {
 
     test('call with context returns result', async () => {
-        // let fakeResult = faker.random.uuid()
-        // let fakeContext = {some: faker.random.uuid()}
-        // let somefun = () => {}
-        // const webWorkerMock = {
-        //     postMessage: () => {webWorkerMock.onmessage({data: [fakeResult,fakeContext]})},
-        // }
-        // global.Worker = jest.fn().mockImplementation(() => webWorkerMock)
-        // global.URL = {createObjectURL: jest.fn()}
-        // let params = {
-        //     fn: () => {return 2},
-        //     args: [2],
-        //     context: {some: faker.random.uuid(), somefun}
-        // }
-        // let result = execute(params)
-        // result = await result
-        // expect(result).toEqual(fakeResult)
-        // expect(params.context).toEqual(Object.assign(fakeContext,{ somefun }))
+        let fakeResult = faker.random.uuid()
+        let fakeContext = {some: faker.random.uuid()}
+        let somefun = () => {}
+        const webWorkerMock = {
+            postMessage: () => {webWorkerMock.onmessage({data: [fakeResult,fakeContext]})},
+        }
+        global.Worker = jest.fn().mockImplementation(() => webWorkerMock)
+        global.URL = {createObjectURL: jest.fn()}
+        let params = {
+            fn: () => {return 2},
+            args: [2],
+            context: {some: faker.random.uuid(), somefun}
+        }
+        let result = execute(params)
+        result = await result
+        expect(result).toEqual(fakeResult)
+        expect(params.context).toEqual(Object.assign(fakeContext,{ somefun }))
     })
 
-    // test('call without context returns result', async () => {
-    //     let fakeResult = faker.random.uuid()
-    //     let fakeContext = {some: faker.random.uuid()}
-    //     const webWorkerMock = {
-    //         postMessage: () => {webWorkerMock.onmessage({data: [fakeResult,fakeContext]})},
-    //     }
-    //     global.Worker = jest.fn().mockImplementation(() => webWorkerMock)
-    //     global.URL = {createObjectURL: jest.fn()}
-    //     let params = {
-    //         fn: () => {return 2},
-    //         args: [2]
-    //     }
-    //     let result = execute(params)
-    //     result = await result
-    //     expect(result).toEqual(fakeResult)
-    //     expect(params.context).toBeUndefined()
-    // })
+    test('call without context returns result', async () => {
+        let fakeResult = faker.random.uuid()
+        let fakeContext = {some: faker.random.uuid()}
+        const webWorkerMock = {
+            postMessage: () => {webWorkerMock.onmessage({data: [fakeResult,fakeContext]})},
+        }
+        global.Worker = jest.fn().mockImplementation(() => webWorkerMock)
+        global.URL = {createObjectURL: jest.fn()}
+        let params = {
+            fn: () => {return 2},
+            args: [2]
+        }
+        let result = execute(params)
+        result = await result
+        expect(result).toEqual(fakeResult)
+        expect(params.context).toBeUndefined()
+    })
 
-    // test('call fails', async () => {
-    //     let params = {
-    //         error: true
-    //     }
-    //     try{
-    //         await execute(params)
-    //         expect(false).toBeTruthy()
-    //     }
-    //     catch(e){
-    //     expect(true).toBeTruthy()
-    //     }
-    // })
+    test('call fails', async () => {
+        let params = {
+            error: true
+        }
+        try{
+            await execute(params)
+            expect(false).toBeTruthy()
+        }
+        catch(e){
+        expect(true).toBeTruthy()
+        }
+    })
 })
--- test/unit/worker-test.js
+++ test/unit/worker-test.js
@@ -5,14 +5,52 @@ function Func(value){
 }
 
 describe('worker should', () => {
-    // beforeAll(() => {
-    //     global.postMessage = jest.fn()
-    // })
-    // beforeEach(() => {
-    //     global.postMessage.mockReset()
-    // })
-    // test('call simple stateless function', async () => {
+    beforeAll(() => {
+        global.postMessage = jest.fn()
+    })
+    beforeEach(() => {
+        global.postMessage.mockReset()
+    })
+    test('call simple stateless function', async () => {
+        let func = function (value) {
+            return value * value
+        }
+        let params ={ data : [{
+            fn: func.toString(),
+            args: [2]
+        }]} 
+        worker()(params)
+        expect(global.postMessage).toBeCalledWith([4, undefined])
+    })
+
+    // test('call simple stateless function with lambda', async () => {
     //     let func = (value) => {
+    //         return value * value
+    //     }
+    //     let params ={ data : [{
+    //         fn: func.toString(),
+    //         args: [2]
+    //     }]} 
+    //     worker()(params)
+    //     expect(global.postMessage).toBeCalledWith([4, undefined])
+    // })
+
+    // test('call simple stateless function with lambda ignores other internal function calls', async () => {
+    //     let func = (value) => {
+    //         function a(){}
+    //         a()
+    //         return value * value
+    //     }
+    //     let params ={ data : [{
+    //         fn: func.toString(),
+    //         args: [2]
+    //     }]} 
+    //     worker()(params)
+    //     expect(global.postMessage).toBeCalledWith([4, undefined])
+    // })
+
+    // test('call simple stateless function with lambda without parenthesis', async () => {
+    //     let func = value => {
     //         return value * value
     //     }
     //     let params ={ data : [{
@@ -57,25 +95,25 @@ describe('worker should', () => {
     //     expect(global.postMessage).toBeCalledWith([8, {value: 8}])
     // })
 
-    test('call class with context and param', async () => {
-        // class someClass {
-        //     constructor(value){
-        //         this.value = value
-        //     }
-        //     foo(some){
-        //         let result = this.value * some
-        //         this.value = result
-        //         return result
-        //     }
-        // }
+    // test('call class with context and param', async () => {
+    //     class someClass {
+    //         constructor(value){
+    //             this.value = value
+    //         }
+    //         foo(some){
+    //             let result = this.value * some
+    //             this.value = result
+    //             return result
+    //         }
+    //     }
         
-        // var instance = new someClass(2)
-        // let params ={ data : [{
-        //     fn: instance.foo.toString(),
-        //     context: instance,
-        //     args: [2]
-        // }]} 
-        // worker()(params)
-        // expect(global.postMessage).toBeCalledWith([4, {value: 4}])
-    })
+    //     var instance = new someClass(2)
+    //     let params ={ data : [{
+    //         fn: instance.foo.toString(),
+    //         context: instance,
+    //         args: [2]
+    //     }]} 
+    //     worker()(params)
+    //     expect(global.postMessage).toBeCalledWith([4, {value: 4}])
+    // })
 })
