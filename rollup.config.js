fbaaf2cadf55968b21063fc3dbd30f7ac60b30b1
kanekotic <me@kanekotic.com>
2022-07-28 13:43:13 +0200

    Alvaro | change rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,9 @@
 import babel from 'rollup-plugin-babel'
-import { uglify } from 'rollup-plugin-uglify'
-import { minify } from 'uglify-es'
 
 export default {
     input: "lib/web-threads.js",
     plugins: [
-        babel(),
-        uglify({},minify)
+        babel()
     ],
     output: [
     {
