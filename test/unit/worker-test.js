2d2e89a289fd90b4d1d199b098e2c9670895ec71
kanekotic <me@kanekotic.com>
2022-07-28 10:56:19 +0200

    Alvaro | fix tests

--- test/unit/worker-test.js
+++ test/unit/worker-test.js
@@ -5,77 +5,77 @@ function Func(value){
 }
 
 describe('worker should', () => {
-    // beforeAll(() => {
-    //     global.postMessage = jest.fn()
-    // })
-    // beforeEach(() => {
-    //     global.postMessage.mockReset()
-    // })
-    // test('call simple stateless function', async () => {
-    //     let func = (value) => {
-    //         return value * value
-    //     }
-    //     let params ={ data : [{
-    //         fn: func.toString(),
-    //         args: [2]
-    //     }]} 
-    //     worker()(params)
-    //     expect(global.postMessage).toBeCalledWith([4, undefined])
-    // })
+    beforeAll(() => {
+        global.postMessage = jest.fn()
+    })
+    beforeEach(() => {
+        global.postMessage.mockReset()
+    })
+    test('call simple stateless function', async () => {
+        let func = function (value) {
+            return value * value
+        }
+        let params ={ data : [{
+            fn: func.toString(),
+            args: [2]
+        }]} 
+        worker()(params)
+        expect(global.postMessage).toBeCalledWith([4, undefined])
+    })
 
-    // test('call function with context', async () => {
-    //     Func.prototype.foo = function(){
-    //         let result = this.value * this.value
-    //         this.value = result
-    //         return result
-    //     };
+    test('call function with context', async () => {
+        Func.prototype.foo = function(){
+            let result = this.value * this.value
+            this.value = result
+            return result
+        };
         
-    //     var instance = new Func(2)
-    //     let params ={ data : [{
-    //         fn: instance.foo.toString(),
-    //         context: instance
-    //     }]} 
-    //     worker()(params)
-    //     expect(global.postMessage).toBeCalledWith([4, {value: 4}])
-    // })
+        var instance = new Func(2)
+        let params ={ data : [{
+            fn: instance.foo.toString(),
+            context: instance
+        }]} 
+        worker()(params)
+        expect(global.postMessage).toBeCalledWith([4, {value: 4}])
+    })
 
-    // test('call function with context and param', async () => {
+    test('call function with context and param', async () => {
         
-    //     Func.prototype.foo2 = function(otherValue){
-    //         let result = this.value * otherValue
-    //         this.value = result
-    //         return result
-    //     };
+        Func.prototype.foo2 = function(otherValue){
+            let result = this.value * otherValue
+            this.value = result
+            return result
+        };
         
-    //     var instance = new Func(2)
-    //     let params ={ data : [{
-    //         fn: instance.foo2.toString(),
-    //         context: instance,
-    //         args: [4]
-    //     }]} 
-    //     worker()(params)
-    //     expect(global.postMessage).toBeCalledWith([8, {value: 8}])
-    // })
+        var instance = new Func(2)
+        let params ={ data : [{
+            fn: instance.foo2.toString(),
+            context: instance,
+            args: [4]
+        }]} 
+        worker()(params)
+        expect(global.postMessage).toBeCalledWith([8, {value: 8}])
+    })
 
     test('call class with context and param', async () => {
-        // class someClass {
-        //     constructor(value){
-        //         this.value = value
-        //     }
-        //     foo(some){
-        //         let result = this.value * some
-        //         this.value = result
-        //         return result
-        //     }
-        // }
+        class someClass {
+            constructor(value){
+                this.value = value
+            }
+            foo(some){
+                let result = this.value * some
+                this.value = result
+                return result
+            }
+        }
         
-        // var instance = new someClass(2)
-        // let params ={ data : [{
-        //     fn: instance.foo.toString(),
-        //     context: instance,
-        //     args: [2]
-        // }]} 
-        // worker()(params)
-        // expect(global.postMessage).toBeCalledWith([4, {value: 4}])
+        var instance = new someClass(2)
+        let params ={ data : [{
+            fn: instance.foo.toString(),
+            context: instance,
+            args: [2]
+        }]} 
+        worker()(params)
+        expect(global.postMessage).toBeCalledWith([4, {value: 4}])
     })
 })
